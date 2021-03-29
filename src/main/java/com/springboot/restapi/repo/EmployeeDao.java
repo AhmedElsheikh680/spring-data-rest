f8ce5d8b723e34427e9cded29da4d4c630e3448b
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-29 20:56:14 +0200

    Spring Data Rest(Configuration, Pagination, Sorting) Closes #2

--- src/main/java/com/springboot/restapi/repo/EmployeeDao.java
+++ src/main/java/com/springboot/restapi/repo/EmployeeDao.java
@@ -2,11 +2,12 @@ package com.springboot.restapi.repo;
 
 import com.springboot.restapi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(path="members")
 public interface EmployeeDao extends JpaRepository<Employee, Integer> {
 
 }
